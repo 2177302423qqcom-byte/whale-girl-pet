0de016e39b2ea9b45f63125c6bb999f3f7e466d9
核桃先生 <2177302423qq.com@gmail.com>
2026-08-17 17:35:25 +0800

    update WhalePet/ChatMsg.cs

--- WhalePet/ChatMsg.cs
+++ WhalePet/ChatMsg.cs
@@ -1,13 +1,22 @@
+using System.ComponentModel;
 using System.Windows;
 
 namespace WhalePet
 {
-    public class ChatMsg
+    public class ChatMsg : INotifyPropertyChanged
     {
+        private string _text = "";
+
         public string Role { get; set; }
-        public string Text { get; set; }
+        public string Text
+        {
+            get => _text;
+            set { _text = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Text))); }
+        }
         public string Meta { get; set; } = "";
         public HorizontalAlignment Align { get; set; } = HorizontalAlignment.Left;
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 
     public class ActivityMsg
